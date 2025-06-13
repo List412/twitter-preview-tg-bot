48ba4ba9d729aa3313bbd6f0f982f76fe09df52a
List412 <erestor412@gmail.com>
2025-06-13 17:46:10 +0500

    expand user struct in incoming messages

--- telegram/types.go
+++ telegram/types.go
@@ -40,6 +40,9 @@ type Voice struct {
 
 type User struct {
 	Id           int    `json:"id"`
+	IsBot        bool   `json:"is_bot"`
+	FirstName    string `json:"first_name"`
+	LastName     string `json:"last_name"`
 	Username     string `json:"username"`
 	LanguageCode string `json:"language_code"`
 }
